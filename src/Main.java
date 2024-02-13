fc5b0d911be961d41200a764a1968e103d4c4196
LOLEMPLOI <sebic1708@gmail.com>
2024-02-13 14:49:50 +0100

    Commit S1-S2
    Ajout des commits de la story 1 et story 2

--- src/Main.java
+++ src/Main.java
@@ -2,39 +2,42 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
+        /** Fonction principale qui permet de lancer le jeu **/
         System.out.println("Hello and welcome!");
-
-        String j1, j2; // declaration des variables Joueur 1 et Joueur 2
-        int longueur,Largeur; // Declaration des variables de la longueur et de la Largeur de notre grille
-
-        Scanner myObj = new Scanner(System.in);  // Créer un scan de l'objet
+        /** Declaration des variables **/
+        String j1, j2;
+        /** Declaration des variables pour la grille **/
+        int longueur,Largeur;
+        /** Declaration des variables pour les pions **/
+        Scanner myObj = new Scanner(System.in);  //Créer un scan de l'objet
 
         System.out.println("le pseudo du joueur 1 ?");
-        j1 = myObj.nextLine();  // lecture du pseudo du joueur 1
+        j1 = myObj.nextLine();  //Lecture du pseudo du joueur 1
 
         System.out.println("le pseudo du joueur 2 ?");
-        j2 = myObj.nextLine();    // lecture du pseudo du joueur 2
+        j2 = myObj.nextLine();    //Lecture du pseudo du joueur 2
 
-        MenuCommencement(); // invocation d'une function pour commencer le jeu
+        MenuCommencement(); //Invocation d'une function pour commencer le jeu
     }
     public static void RegleMenu(){
+        /** Fonction qui affiche les regles du jeu et propose de retourner au menu principal ou de reprendre la partie **/
         Scanner scanner = new Scanner(System.in);
-                // tous les choix disponible
+            //Tout les choix disponibles
             System.out.println("Sélectionnez une action :");
             System.out.println("1. Retour au menu principal !");
             System.out.println("2. Reprendre la partie !");
-
+            /** Lecture du choix **/
             int choix = scanner.nextInt();
 
             switch (choix) {
-                case 1: // choix 1 on retourne au menu de base
+                case 1: //Choix 1 on retourne au menu de base
                     MenuCommencement();
                     break;
-                case 2: // choix 2 on retourne a notre games si on est en games et sinon ça nous retourne au menu si on est au menu
+                case 2: //Choix 2 on retourne a notre games si on est en games et sinon ça nous retourne au menu si on est au menu
                     System.out.println("Au revoir !");
                     scanner.close();
                     System.exit(0);
-                default: // choix par defaut si on choisie quelque chose de non valide
+                default: //Choix par defaut si on choisie quelque chose de non valide
                     System.out.println("Choix non valide. Veuillez sélectionner une action valide.");
             }
 
@@ -42,17 +45,17 @@ public class Main {
     public static void MenuCommencement(){
         Scanner scanner = new Scanner(System.in);
 
-            // affichage de tous les choix possible
+            //Affichage de tous les choix possible
             System.out.println("Sélectionnez une action :");
             System.out.println("1. Regle du jeu !");
             System.out.println("2. Nouvelle Partie !");
             System.out.println("3. Charger une sauvegarde !");
             System.out.println("4. Quitter");
 
-            int choix = scanner.nextInt(); // lecture de notre choix
+            int choix = scanner.nextInt(); //Lecture de notre choix
 
             switch (choix) {
-                case 1: // choix 1 affiche les regles et invoque la function regle menu
+                case 1: //Choix 1 affiche les regles et invoque la function regle menu
                     System.out.println("Les Regles du jeu :");
 
                     System.out.println("Pendant son tour un joueur peut déplacer son pion d’une case ");
@@ -65,15 +68,15 @@ public class Main {
                     System.out.println("Nouvelle partie ...");
                     // Ajoutez ici le code correspondant à l'Action 2
                     break;
-                case 3: // choix 3 charge une partie déjà sauvegarder
+                case 3: //Choix 3 charge une partie déjà sauvegarder
                     System.out.println("Chargement d'une sauvegarde");
-                    // Ajoutez ici le code correspondant à l'Action 2
+                    //Ajoutez ici le code correspondant à l'Action 2
                     break;
-                case 4: // choix 4 quitte le jeu
+                case 4: //Choix 4 quitte le jeu
                     System.out.println("Au revoir !");
                     scanner.close();
                     System.exit(0);
-                default: // choix par defaut si vous mettez quelque chose de pas valide
+                default: //Choix par defaut si vous mettez quelque chose de pas valide
                     System.out.println("Choix non valide. Veuillez sélectionner une action valide.");
             }
 
