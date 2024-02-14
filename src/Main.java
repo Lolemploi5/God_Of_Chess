98ced9f2f3d5c30c5e285015631e6557dc802089
Kiria <sirvos.lol@gmail.com>
2024-02-14 16:22:10 +0100

    classe modif

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main {
         int longueur,Largeur;
         /* Declaration des variables pour les pions **/
 
-        MenuCommencement(); //Invocation d'une fonction pour commencer le jeu
+        Menus.MenuCommencement(); //Invocation d'une fonction pour commencer le jeu
 
 
     }
@@ -29,7 +29,7 @@ public class Main {
             System.out.printf(ConsoleColors.WHITE_BRIGHT + "\nEntrez la lettre et le nombre de la case (ex. C:9) : " + ConsoleColors.RESET);
             input = scanner.nextLine().toUpperCase();
 
-            if (input.matches("[A-J]:[1-11]")) {
+            if (input.matches("[A-J]:[1-9]")) {
                 lettre = input.charAt(0);
                 nombre = Integer.parseInt(input.substring(2));
                 break;
@@ -52,7 +52,7 @@ public class Main {
             //Affichage de tous les choix possible
             System.out.println();
             System.out.println(ConsoleColors.WHITE_BRIGHT + "Faites votre choix : " + LePlateau.NomJoueur() + ConsoleColors.RESET);
-            System.out.println(ConsoleColors.YELLOW_BOLD + "/help pour le menu!" + ConsoleColors.RESET);
+            System.out.println(ConsoleColors.YELLOW_BOLD + "/aide pour le menu!" + ConsoleColors.RESET);
             System.out.println(ConsoleColors.BLUE_BOLD + "/haut , /bas , /droite , /gauche" + ConsoleColors.RESET);
             System.out.printf(ConsoleColors.WHITE_BRIGHT + "Entrez la commande de votre choix : " + ConsoleColors.RESET);
 
@@ -64,7 +64,7 @@ public class Main {
             choix = scanner.nextLine().toLowerCase(); // Lecture de notre choix (converti en minuscules pour simplifier la comparaison)
 
             switch (choix) {
-                case "/help": //Choix 1 affiche les regles et invoque la function regle menu
+                case "/aide": //Choix 1 affiche les regles et invoque la function regle menu
                     System.out.println();
                     System.out.println(ConsoleColors.YELLOW_BOLD_BRIGHT + "Les Règles du jeu :" + ConsoleColors.RESET);
 
@@ -75,7 +75,17 @@ public class Main {
                     System.out.println(ConsoleColors.WHITE_BRIGHT + "- Un joueur ne peut pas détruire une case occupée." + ConsoleColors.RESET);
                     System.out.println(ConsoleColors.WHITE_BRIGHT + "- Un joueur ne peut pas occuper une case détruite ou une case deja occupée." + ConsoleColors.RESET);
                     System.out.printf(ConsoleColors.WHITE_BOLD_BRIGHT + "- Un joueur bloqué pendant un tour est déclaré " + ConsoleColors.RED_BOLD_BRIGHT+ "perdant" + ConsoleColors.WHITE_BOLD_BRIGHT + "!" + ConsoleColors.RESET);
+                    System.out.println(" \n");
+                    System.out.println(ConsoleColors.YELLOW_BOLD_BRIGHT + "Les Commandes dans le jeu :" + ConsoleColors.RESET);
+
+                    System.out.println(ConsoleColors.GREEN_BOLD_BRIGHT + "/s " + ConsoleColors.RESET + ConsoleColors.WHITE_BRIGHT+ "pour sauvegarder." + ConsoleColors.RESET);
+                    System.out.println(ConsoleColors.RED_BOLD_BRIGHT + "/q " + ConsoleColors.RESET + ConsoleColors.WHITE_BRIGHT+ "pour quitter le jeu." + ConsoleColors.RESET);
+                    System.out.println(ConsoleColors.BLUE_BOLD_BRIGHT + "/h " + ConsoleColors.RESET + ConsoleColors.WHITE_BRIGHT+ "pour monter d'une case." + ConsoleColors.RESET);
+                    System.out.println(ConsoleColors.BLUE_BOLD_BRIGHT + "/b " + ConsoleColors.RESET + ConsoleColors.WHITE_BRIGHT+ "pour baisser d'une case." + ConsoleColors.RESET);
+                    System.out.println(ConsoleColors.BLUE_BOLD_BRIGHT + "/d " + ConsoleColors.RESET + ConsoleColors.WHITE_BRIGHT+ "pour allez a droite ." + ConsoleColors.RESET);
+                    System.out.println(ConsoleColors.BLUE_BOLD_BRIGHT + "/g " + ConsoleColors.RESET + ConsoleColors.WHITE_BRIGHT+ "pour allez a gauche." + ConsoleColors.RESET);
                     System.out.println();
+
                     Jeu(LePlateau);
 
                     break;
@@ -140,142 +150,6 @@ public class Main {
 
         } while (choix.length() != 5);
     }
-    public static void MenuJeu(Joueur MyJoueur){
-        Scanner scanner = new Scanner(System.in);
-        int choix;
-        do {
-            //Affichage de tous les choix possible
-            System.out.println();
-            System.out.println(ConsoleColors.WHITE_BRIGHT + "Faites votre choix : " + MyJoueur.getPseudo() + ConsoleColors.RESET);
-            System.out.println(ConsoleColors.YELLOW_BOLD + "1. Règles du jeu !" + ConsoleColors.RESET);
-            System.out.println(ConsoleColors.BLUE_BOLD + "2. Sauvegarder la partie !" + ConsoleColors.RESET);
-            System.out.println(ConsoleColors.RED_BOLD + "3. Quitter la partie" + ConsoleColors.RESET);
-            System.out.println(ConsoleColors.GREEN_BRIGHT + "4. Revenir a la partie" + ConsoleColors.RESET);
-            System.out.printf(ConsoleColors.WHITE_BRIGHT + "Entrez le numéro de votre choix : " + ConsoleColors.RESET);
-
-            while (!scanner.hasNextInt()) {
-                System.out.println(ConsoleColors.RED_BOLD_BRIGHT + "Erreur : Vous devez entrer un nombre." + ConsoleColors.RESET);
-                scanner.next();
-            }
-
-            choix = scanner.nextInt(); //Lecture de notre choix
-
-            switch (choix) {
-                case 1: //Choix 1 affiche les regles et invoque la function regle menu
-                    System.out.println();
-                    System.out.println(ConsoleColors.YELLOW_BOLD_BRIGHT + "Les Règles du jeu :" + ConsoleColors.RESET);
-
-                    System.out.println(ConsoleColors.WHITE_BRIGHT + "Pendant son tour un joueur peut déplacer son pion d’une case " + ConsoleColors.RESET);
-                    System.out.println(ConsoleColors.WHITE_BRIGHT + "(verticalement ou horizontalement), puis il détruit une case du plateau." + ConsoleColors.RESET);
-                    System.out.println(ConsoleColors.WHITE_BOLD_BRIGHT + "Le dernier joueur pouvant encore se déplacer" + ConsoleColors.GREEN_BOLD_BRIGHT + " gagne" + ConsoleColors.WHITE_BOLD_BRIGHT + "!" + ConsoleColors.RESET);
-                    System.out.println(ConsoleColors.YELLOW_BOLD_BRIGHT + "Contraintes :" + ConsoleColors.RESET);
-                    System.out.println(ConsoleColors.WHITE_BRIGHT + "- Un joueur ne peut pas détruire une case occupée." + ConsoleColors.RESET);
-                    System.out.println(ConsoleColors.WHITE_BRIGHT + "- Un joueur ne peut pas occuper une case détruite ou une case deja occupée." + ConsoleColors.RESET);
-                    System.out.printf(ConsoleColors.WHITE_BOLD_BRIGHT + "- Un joueur bloqué pendant un tour est déclaré " + ConsoleColors.RED_BOLD_BRIGHT+ "perdant" + ConsoleColors.WHITE_BOLD_BRIGHT + "!" + ConsoleColors.RESET);
-                    System.out.println();
-                    MenuJeu(MyJoueur);
-
-                    break;
-                case 2: //Choix 3 charge une partie déjà sauvegarder
-                    System.out.println();
-                    System.out.println(ConsoleColors.BLUE_BOLD + "Creation d'une sauvegarde" + ConsoleColors.RESET);
-
-                    break;
-                case 3: //Choix 4 quitte le jeu
-                    System.out.println(ConsoleColors.RED_BOLD + "Au revoir !" + ConsoleColors.RESET);
-                    scanner.close();
-                    System.exit(0);
-                    break;
-
-                case 4: //Choix 4 Revenir a la game
-                    System.out.println();
-                    MenuJeu(MyJoueur);
 
 
-                default: //Choix par defaut si vous mettez quelque chose de pas valide
-                    System.out.println(ConsoleColors.RED_BOLD_BRIGHT + "Erreur : Choix non valide. Veuillez sélectionner une action valide." + ConsoleColors.RESET);
-            }
-
-        } while (choix != 4);
-    }
-
-    public static void MenuCommencement(){
-        Scanner scanner = new Scanner(System.in);
-            int choix;
-        do {
-            //Affichage de tous les choix possible
-            System.out.println();
-            System.out.println(ConsoleColors.WHITE_BRIGHT + "Faites votre choix :" + ConsoleColors.RESET);
-            System.out.println(ConsoleColors.YELLOW_BOLD + "1. Règles du jeu !" + ConsoleColors.RESET);
-            System.out.println(ConsoleColors.GREEN_BOLD + "2. Nouvelle Partie !" + ConsoleColors.RESET);
-            System.out.println(ConsoleColors.BLUE_BOLD + "3. Charger une sauvegarde !" + ConsoleColors.RESET);
-            System.out.println(ConsoleColors.RED_BOLD + "4. Quitter" + ConsoleColors.RESET);
-            System.out.printf(ConsoleColors.WHITE_BRIGHT + "Entrez le numéro de votre choix : " + ConsoleColors.RESET);
-
-            while (!scanner.hasNextInt()) {
-                System.out.println(ConsoleColors.RED_BOLD_BRIGHT + "Erreur : Vous devez entrer un nombre (1-4)." + ConsoleColors.RESET);
-                MenuCommencement();
-                scanner.next();
-            }
-
-            choix = scanner.nextInt(); //Lecture de notre choix
-
-            switch (choix) {
-                case 1: //Choix 1 affiche les regles et invoque la function regle menu
-                    System.out.println();
-                    System.out.println(ConsoleColors.YELLOW_BOLD_BRIGHT + "Les Règles du jeu :" + ConsoleColors.RESET);
-
-                    System.out.println(ConsoleColors.WHITE_BRIGHT + "Pendant son tour un joueur peut déplacer son pion d’une case " + ConsoleColors.RESET);
-                    System.out.println(ConsoleColors.WHITE_BRIGHT + "(verticalement ou horizontalement), puis il détruit une case du plateau." + ConsoleColors.RESET);
-                    System.out.println(ConsoleColors.WHITE_BOLD_BRIGHT + "Le dernier joueur pouvant encore se déplacer" + ConsoleColors.GREEN_BOLD_BRIGHT + " gagne" + ConsoleColors.WHITE_BOLD_BRIGHT + "!" + ConsoleColors.RESET);
-                    System.out.println(ConsoleColors.YELLOW_BOLD_BRIGHT + "Contraintes :" + ConsoleColors.RESET);
-                    System.out.println(ConsoleColors.WHITE_BRIGHT + "- Un joueur ne peut pas détruire une case occupée." + ConsoleColors.RESET);
-                    System.out.println(ConsoleColors.WHITE_BRIGHT + "- Un joueur ne peut pas occuper une case détruite ou une case deja occupée." + ConsoleColors.RESET);
-                    System.out.printf(ConsoleColors.WHITE_BOLD_BRIGHT + "- Un joueur bloqué pendant un tour est déclaré " + ConsoleColors.RED_BOLD_BRIGHT+ "perdant" + ConsoleColors.WHITE_BOLD_BRIGHT + "!" + ConsoleColors.RESET);
-                    System.out.println();
-                    MenuCommencement();
-
-                    break;
-                case 2: // choix 2 créer une nouvelle partie
-
-                    /* Declaration des variables **/
-                    String j1Pseudo, j2Pseudo;
-                    System.out.println();
-                    System.out.println(ConsoleColors.GREEN_BOLD_BRIGHT + "Nouvelle partie ..." + ConsoleColors.RESET);
-                    Scanner myObj = new Scanner(System.in);  //Créer un scan de l'objet
-
-                    System.out.println(ConsoleColors.WHITE_BOLD_BRIGHT + "Entrez le pseudo du joueur 1 ?" + ConsoleColors.RESET);
-                    j1Pseudo = myObj.nextLine();  //Lecture du pseudo du joueur 1
-                    Joueur joueur1 = new Joueur(5,5,j1Pseudo);
-
-
-                    System.out.println(ConsoleColors.WHITE_BOLD_BRIGHT + "Entrez le pseudo du joueur 2 ?" + ConsoleColors.RESET);
-                    j2Pseudo = myObj.nextLine();    //Lecture du pseudo du joueur 2
-
-                    Joueur joueur2 = new Joueur(5,4, j2Pseudo);
-
-                    System.out.println();
-                    System.out.println(ConsoleColors.GREEN_BOLD_BRIGHT + "La partie commence !" + ConsoleColors.RESET);
-                    Plateau monPlateau = new Plateau(11,10,joueur1,joueur2);
-                    Jeu(monPlateau);
-                    break;
-                case 3: //Choix 3 charge une partie déjà sauvegarder
-                    System.out.println();
-                    System.out.println(ConsoleColors.BLUE_BOLD + "Chargement d'une sauvegarde" + ConsoleColors.RESET);
-
-                    break;
-                case 4: //Choix 4 quitte le jeu
-                    System.out.println(ConsoleColors.RED_BOLD_BRIGHT + "Au revoir !" + ConsoleColors.RESET);
-                    scanner.close();
-                    System.exit(0);
-
-                default: //Choix par defaut si vous mettez quelque chose de pas valide
-                    System.out.println(ConsoleColors.RED_BOLD_BRIGHT + "Erreur : Choix non valide. Veuillez sélectionner une action valide." + ConsoleColors.RESET);
-            }
-
-    }
-
-
-        while (choix != 3);
-    }
 }
--- src/Menus.java
+++ src/Menus.java
@@ -1,2 +1,81 @@
-package PACKAGE_NAME;public class Menu {
+import java.util.Scanner;
+
+public class Menus {
+    public static void MenuCommencement(){
+        Scanner scanner = new Scanner(System.in);
+        int choix;
+        do {
+            //Affichage de tous les choix possible
+            System.out.println();
+            System.out.println(ConsoleColors.WHITE_BRIGHT + "Faites votre choix :" + ConsoleColors.RESET);
+            System.out.println(ConsoleColors.YELLOW_BOLD + "1. Règles du jeu !" + ConsoleColors.RESET);
+            System.out.println(ConsoleColors.BLUE_BOLD_BRIGHT + "2. Nouvelle Partie !" + ConsoleColors.RESET);
+            System.out.println(ConsoleColors.GREEN_BOLD_BRIGHT + "3. Charger une sauvegarde !" + ConsoleColors.RESET);
+            System.out.println(ConsoleColors.RED_BOLD + "4. Quitter" + ConsoleColors.RESET);
+            System.out.printf(ConsoleColors.WHITE_BRIGHT + "Entrez le numéro de votre choix : " + ConsoleColors.RESET);
+
+            while (!scanner.hasNextInt()) {
+                System.out.println(ConsoleColors.RED_BOLD_BRIGHT + "Erreur : Vous devez entrer un nombre (1-4)." + ConsoleColors.RESET);
+                MenuCommencement();
+                scanner.next();
+            }
+
+            choix = scanner.nextInt(); //Lecture de notre choix
+
+            switch (choix) {
+                case 1: //Choix 1 affiche les regles et invoque la function regle menu
+                    System.out.println();
+                    System.out.println(ConsoleColors.YELLOW_BOLD_BRIGHT + "Les Règles du jeu :" + ConsoleColors.RESET);
+
+                    System.out.println(ConsoleColors.WHITE_BRIGHT + "Pendant son tour un joueur peut déplacer son pion d’une case " + ConsoleColors.RESET);
+                    System.out.println(ConsoleColors.WHITE_BRIGHT + "(verticalement ou horizontalement), puis il détruit une case du plateau." + ConsoleColors.RESET);
+                    System.out.println(ConsoleColors.WHITE_BOLD_BRIGHT + "Le dernier joueur pouvant encore se déplacer" + ConsoleColors.GREEN_BOLD_BRIGHT + " gagne" + ConsoleColors.WHITE_BOLD_BRIGHT + "!" + ConsoleColors.RESET);
+                    System.out.println(ConsoleColors.YELLOW_BOLD_BRIGHT + "Contraintes :" + ConsoleColors.RESET);
+                    System.out.println(ConsoleColors.WHITE_BRIGHT + "- Un joueur ne peut pas détruire une case occupée." + ConsoleColors.RESET);
+                    System.out.println(ConsoleColors.WHITE_BRIGHT + "- Un joueur ne peut pas occuper une case détruite ou une case deja occupée." + ConsoleColors.RESET);
+                    System.out.printf(ConsoleColors.WHITE_BOLD_BRIGHT + "- Un joueur bloqué pendant un tour est déclaré " + ConsoleColors.RED_BOLD_BRIGHT+ "perdant" + ConsoleColors.WHITE_BOLD_BRIGHT + "!" + ConsoleColors.RESET);
+                    System.out.println();
+                    MenuCommencement();
+
+                    break;
+                case 2: // choix 2 créer une nouvelle partie
+
+                    /* Declaration des variables **/
+                    String j1Pseudo, j2Pseudo;
+                    System.out.println();
+                    System.out.println(ConsoleColors.GREEN_BOLD_BRIGHT + "Nouvelle partie ..." + ConsoleColors.RESET);
+                    Scanner myObj = new Scanner(System.in);  //Créer un scan de l'objet
+
+                    System.out.println(ConsoleColors.WHITE_BOLD_BRIGHT + "Entrez le pseudo du joueur 1 ?" + ConsoleColors.RESET);
+                    j1Pseudo = myObj.nextLine();  //Lecture du pseudo du joueur 1
+                    Joueur joueur1 = new Joueur(5,5,j1Pseudo);
+
+
+                    System.out.println(ConsoleColors.WHITE_BOLD_BRIGHT + "Entrez le pseudo du joueur 2 ?" + ConsoleColors.RESET);
+                    j2Pseudo = myObj.nextLine();    //Lecture du pseudo du joueur 2
+
+                    Joueur joueur2 = new Joueur(5,4, j2Pseudo);
+
+                    System.out.println();
+                    System.out.println(ConsoleColors.GREEN_BOLD_BRIGHT + "La partie commence !" + ConsoleColors.RESET);
+                    Plateau monPlateau = new Plateau(11,10,joueur1,joueur2);
+                    Main.Jeu(monPlateau);
+                    break;
+                case 3: //Choix 3 charge une partie déjà sauvegarder
+                    System.out.println();
+                    System.out.println(ConsoleColors.BLUE_BOLD + "Chargement d'une sauvegarde" + ConsoleColors.RESET);
+
+                    break;
+                case 4: //Choix 4 quitte le jeu
+                    System.out.println(ConsoleColors.RED_BOLD_BRIGHT + "Au revoir !" + ConsoleColors.RESET);
+                    scanner.close();
+                    System.exit(0);
+
+                default: //Choix par defaut si vous mettez quelque chose de pas valide
+                    System.out.println(ConsoleColors.RED_BOLD_BRIGHT + "Erreur : Choix non valide. Veuillez sélectionner une action valide." + ConsoleColors.RESET);
+            }
+
+        } while (choix != 3);
+    }
+
 }
